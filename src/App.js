fd420466843b0412b14eec39c7983a952256e47a
SamG10 <samuel100800@gmail.com>
2022-02-04 17:46:53 +0100

    test hashRouter

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 // import logo from './logo.svg';
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+// import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home.jsx';
 
 function App() {
   return <>
-    <BrowserRouter>
+    <HashRouter>
       <Navbar/>
         <div>
           <Routes>
             <Route exact path="/" element={<Home />} />
           </Routes>
         </div>
-    </BrowserRouter>
+    </HashRouter>
   </>
 }
 
