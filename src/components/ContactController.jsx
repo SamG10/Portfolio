748ea1b01fc5fbf4e1282b8d55c33f4beb6f887c
SamG10 <samuel100800@gmail.com>
2022-02-16 09:55:25 +0100

    change footer

--- src/components/ContactController.jsx
+++ src/components/ContactController.jsx
@@ -9,14 +9,14 @@ const ContactController = () => {
     return <>
          <h1 id="contacth1">Contact</h1>
     <Contact/>
-        <footer>
+        <div className="footer">
             <div className="img_social">
                 <a href="https://github.com/SamG10"><i className="fab fa-github"></i></a> 
                 <a href="https://www.linkedin.com/in/samuel-george-10/"><i className="fab fa-linkedin"></i></a> 
             </div>
  
             <p>Copyright Samuel George. &copy; 2022</p>
-        </footer>
+        </div>
         
     </>
 }
